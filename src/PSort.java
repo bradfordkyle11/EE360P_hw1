17f6da05040c76752073f4c5941b451a25df8022
Spencer Yue <spenceryue@gmail.com>
2018-01-31 12:58:46 -0600

    index-1 >> index

--- src/PSort.java
+++ src/PSort.java
@@ -6,15 +6,15 @@ import java.util.concurrent.*;
 
 public class PSort{
   public static void parallelSort(int[] A, int begin, int end){
-    // TODO: Implement your parallel sort function 
+    // TODO: Implement your parallel sort function
 	  ForkJoinPool forkJoinPool = new ForkJoinPool();
 	  QuickSort qs = new QuickSort(A, begin, end);
 	  forkJoinPool.invoke(qs);
-	  
-	  
-	
+
+
+
   }
-  
+
 
 }
 
@@ -37,7 +37,7 @@ class QuickSort extends RecursiveTask<int[]>{
 		}
 		else {
 			int index = getSplitIndex(A, begin, end);
-			QuickSort qs1 = new QuickSort(A, begin, index-1);
+			QuickSort qs1 = new QuickSort(A, begin, index);
 			qs1.fork();
 			QuickSort qs2 = new QuickSort(A, index+1, end);
 			int[] A2 = qs2.compute();
@@ -48,7 +48,7 @@ class QuickSort extends RecursiveTask<int[]>{
 			return A;
 		}
 	}
-	
+
 	int getSplitIndex(int[] A, int begin, int end) {
 		int pivot = A[begin];
 		int i = begin - 1;
@@ -67,8 +67,8 @@ class QuickSort extends RecursiveTask<int[]>{
 		}
 		return i + 1;
 	}
-	
-	
+
+
 	int[] seqInsertSort(int[] A, int begin, int end) {
 		for (int i = begin + 1; i < end; i++) {
 			int j = i - 1;
@@ -77,7 +77,7 @@ class QuickSort extends RecursiveTask<int[]>{
 				A[j + 1] = A[j];
 				A[j] = current;
 				j--;
-			}			
+			}
 		}
 		return A;
 	}
